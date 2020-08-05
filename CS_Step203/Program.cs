e47463836702b774f08a24a596865b8e62b92590
Gavin Kimberlin <guviner@gmail.com>
2020-08-05 04:18:30 -0700

    Create a program that uses lambda functions to create new lists.

--- CS_Step203/Program.cs
+++ CS_Step203/Program.cs
@@ -10,6 +10,43 @@ namespace CS_Step203
     {
         static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>();
+            { 
+                employees.Add(new Employee { FirstName = "Jerry", LastName = "Smith", Id = 0});
+                employees.Add(new Employee { FirstName = "Bill", LastName = "Johnson", Id = 1});
+                employees.Add(new Employee { FirstName = "Jim", LastName = "Williams", Id = 2});
+                employees.Add(new Employee { FirstName = "Joe", LastName = "Brown", Id = 3});
+                employees.Add(new Employee { FirstName = "Dwight", LastName = "Jones", Id = 4});
+                employees.Add(new Employee { FirstName = "Karen", LastName = "Garcia", Id = 5});
+                employees.Add(new Employee { FirstName = "Felix", LastName = "Miller", Id = 6});
+                employees.Add(new Employee { FirstName = "Gary", LastName = "Davis", Id = 7});
+                employees.Add(new Employee { FirstName = "Dan", LastName = "Rodriguez", Id = 8});
+                employees.Add(new Employee { FirstName = "Joe", LastName = "Martinez", Id = 9});
+            }
+
+            List<Employee> Joes = new List<Employee>();
+            foreach ( var Employee in employees)
+            {
+                if (Employee.FirstName == "Joe")
+                {
+                    Joes.Add(Employee);
+                    Console.WriteLine(Employee.FirstName + " " + Employee.LastName + " was added to the Joes list.");
+                }
+            }
+
+            List<Employee> Joes2 = employees.FindAll(Employee => Employee.FirstName == "Joe");
+            foreach(var Employee in Joes2)
+            {
+                Console.WriteLine(Employee.FirstName + " " + Employee.LastName + " was added to the second Joes list.");
+            }
+
+            List<Employee> idGreaterThan5 = employees.FindAll(Employee => Employee.Id > 5);
+            foreach (var Employee in idGreaterThan5)
+            {
+                Console.WriteLine(Employee.FirstName + " " + Employee.LastName + " was added to the ID greater than 5 list.");
+            }
+
+            Console.ReadLine();
         }
     }
 }
